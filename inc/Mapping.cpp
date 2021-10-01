38842f0f0f8b1d2e29fbac3da80c1dbaab1ec8e4
Raman Sehgal <sc.ramansehgal@gmail.com>
2021-10-01 16:37:05 +0530

    Trying to add Mapping file for HV and digitizer

---
+++ inc/Mapping.cpp
@@ -0,0 +1,8 @@
+/*
+**	Filename : Mapping.cpp
+**	2021-10-01
+**	username : rsehgal
+*/
+#include "Mapping.h"
+#include <string>
+std::vector<std::string> vecOfPsBars={};
---
+++ inc/Mapping.h
@@ -0,0 +1,13 @@
+/*
+**	Filename : Mapping.h
+**	2021-10-01
+**	username : rsehgal
+*/
+#ifndef Mapping_h
+#define Mapping_h
+#include <string>
+extern std::vector<std::string> vecOfPsBars;
+extern std::vector<unsigned short> vecOfSlot;
+extern std::vector<unsigned short> vecOfChannel;
+extern std::vector<unsigned short> vecOfPowerSupply;
+#endif
